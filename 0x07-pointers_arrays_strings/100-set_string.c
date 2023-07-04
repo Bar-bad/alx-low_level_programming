12260daa042797e3ef8d896ec73c14294c337183
Malosoy <faithbar116@gmail.com>
2023-07-04 14:34:04 +0300

    Advanced tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - function that sets the value o a pointer to a char
+ * @s: a pointer to a pointer
+ * @to: a pointer
+ *
+ * Return: Always (0)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,4 +6,23 @@
  * @a: input
  * @size: input
  *
- * Return:  Always  
+ * Return: Always ()
+ */
+void print_diagsums(int *a, int size)
+{
+	int diag_sum1, diag_sum2, i;
+
+	diag_sum1 = 0;
+	diag_sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diag_sum1 = diag_sum1 + a[i * size + i];
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		diag_sum2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", diag_sum1, diag_sum2);
+}
