eda47d18f35069d1b9206b39ddf681c687710704
Malosoy <faithbar116@gmail.com>
2023-07-07 03:20:14 +0300

    Chenge prototype in `main.h` file from `sqrt_recursion` to `_sqrt_recursion`

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
-int recurs_sqrt_recursion(int n, int i);
+
+int recurs_sqrt_recursion(int n, int a);
+
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: the number to calculate the square root of
@@ -10,7 +12,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (recurs_sqrt_recursion(n, 0));
 }
 
 /**
@@ -27,5 +29,5 @@ int recurs_sqrt_recursion(int n, int a)
 		return (-1);
 	if (a * a == n)
 		return (a);
-	return (recurs_sqrt_recuesion(n, a + 1));
+	return (recurs_sqrt_recursion(n, a + 1));
 }
