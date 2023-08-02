786d060d58c5e8ac7379dba5e1e9997987ffe076
Malosoy <faithbar116@gmail.com>
2023-08-02 16:01:12 +0300

    Ammend codes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - function that prints its own opcodes
+ * printout_opcodes - function that prints its own opcodes
+ * @myFunction: a const char
+ * @theBytes: the bytes
+ *
+ * Return: Always 0 (Success)
+ */
+void printout_opcodes(const char *myFunction, int theBytes)
+{
+	const unsigned char *theopcodes = (const unsigned char *)myFunction;
+	int count;
+
+	for (count = 0; theopcodes[count] != '\0'; count++)
+	{
+		if (count == theBytes - 1)
+		{
+			printf("%02hx\n", theopcodes[count]);
+			break;
+		}
+		printf("%02hx ", theopcodes[count]);
+	}
+}
+
+/**
+ * main - start of main function
  * @argc: the number of arguments
  * @argv: an array of the arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int theBytes, count;
-	char *array;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 
-	theBytes = atoi(argv[1]);
+	int theBytes = atoi(argv[1]);
 
 	if (theBytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
-
-	array = (char *)main;
-
-	for (count = 0; count < theBytes; count++)
-	{
-		if (count == theBytes - 1)
-		{
-			printf("%02hhx\n", array[count]);
-			break;
-		}
-		printf("%02hhx", array[count]);
-	}
+	printout_opcodes((const char *)main, theBytes);
 	return (0);
 }
