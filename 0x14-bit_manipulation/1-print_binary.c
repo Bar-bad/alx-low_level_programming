3b0dc3c362b36a21e75b4023d8a01100da1dfdf1
Bar-bad <faithbar116@gmail.com>
2023-09-28 19:33:26 -0700

    files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_binary - function that prints the binary equivalent
+ * of a decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	int lcount, counter = 0;
+	unsigned long int 
