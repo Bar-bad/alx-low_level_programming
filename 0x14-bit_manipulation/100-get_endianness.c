31e28ad5cded7a509e142d2b67b627e9927219ee
Bar-bad <faithbar116@gmail.com>
2023-09-28 20:17:12 -0700

    Error fixes

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * get_endianness - function tha checks if a machine is little or big endian
+ *
  * Return: For big, 0,
  * For little, 1
- * */
-int get_endianess(void)
+ */
+int get_endianness(void)
 {
 	char *bett = (char *) &lcount;
 	unsigned int lcount = 1;
